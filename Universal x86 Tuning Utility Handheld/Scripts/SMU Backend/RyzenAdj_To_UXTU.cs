15a739f822a511ef8cbda105bfacddf1b6995ee4
James Craddock-Jones <jamescraddockjones@icloud.com>
2023-08-06 16:30:04 +0100

    Update SMU backend

--- Universal x86 Tuning Utility Handheld/Scripts/SMU Backend/RyzenAdj_To_UXTU.cs
+++ Universal x86 Tuning Utility Handheld/Scripts/SMU Backend/RyzenAdj_To_UXTU.cs
@@ -1,18 +1,21 @@
-﻿using System;
+﻿using RyzenSmu;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using Universal_x86_Tuning_Utility_Handheld.Scripts.Intel;
 
 namespace Universal_x86_Tuning_Utility.Scripts.AMD_Backend
 {
 
     internal class RyzenAdj_To_UXTU
     {
+        static int i = 0;
         //Translate RyzenAdj like cli arguments to UXTU
-        public static void Translate(string _ryzenAdjString)
+        public static async void Translate(string _ryzenAdjString, bool isAutoReapply = false)
         {
             try
             {
@@ -20,38 +23,35 @@ namespace Universal_x86_Tuning_Utility.Scripts.AMD_Backend
                 _ryzenAdjString = _ryzenAdjString.Substring(0, _ryzenAdjString.Length - 1);
                 //Split cli arguments into array
                 string[] ryzenAdjCommands = _ryzenAdjString.Split(' ');
-                List<Task> tasks = new List<Task>();
-                int i = 0;
-                do
+                ryzenAdjCommands = ryzenAdjCommands.Distinct().ToArray();
+
+                //Run through array
+                foreach (string ryzenAdjCommand in ryzenAdjCommands)
                 {
-                    //Run through array
-                    foreach (string ryzenAdjCommand in ryzenAdjCommands)
+                    await Task.Run(() =>
                     {
-                        tasks.Add(Task.Run(async () =>
+                        try
                         {
-                            try
-                            {
-                                // Extract the command string before the "=" sign
-                                string ryzenAdjCommandString = ryzenAdjCommand.Split('=')[0].Replace("=", null).Replace("--", null);
+                            string command = ryzenAdjCommand;
+                            if (!command.Contains("=")) command = ryzenAdjCommand + "=0";
+                            // Extract the command string before the "=" sign
+                            string ryzenAdjCommandString = command.Split('=')[0].Replace("=", null).Replace("--", null);
+                            // Extract the command string after the "=" sign
+                            string ryzenAdjCommandValueString = command.Substring(ryzenAdjCommand.IndexOf('=') + 1);
 
-                                //Convert value of select cli argument to int
-                                string ryzenAdjCommandValueString = ryzenAdjCommand.Substring(ryzenAdjCommand.IndexOf('=') + 1);
-                                int ryzenAdjCommandValue = Convert.ToInt32(ryzenAdjCommandValueString);
+                            //Convert value of select cli argument to uint
+                            uint ryzenAdjCommandValue = Convert.ToUInt32(ryzenAdjCommandValueString);
+                            if (ryzenAdjCommandValue <= 0 && !ryzenAdjCommandString.Contains("co")) SMUCommands.applySettings(ryzenAdjCommandString, 0x0);
+                            else SMUCommands.applySettings(ryzenAdjCommandString, ryzenAdjCommandValue);
+                            Task.Delay(2);
 
-                                SMUCommands.applySettings(ryzenAdjCommandString, (uint)ryzenAdjCommandValue);
-                                await Task.Delay(2);
-                            }
-                            catch (Exception ex) { }
-                        }));
-
-                        Task.WaitAll(tasks.ToArray());
-
-                        i++;
-                    }
-                } while (i < 2);
-
+                        }
+                        catch (Exception ex) { }
+                    });
+                }
             }
             catch (Exception ex) { }
         }
     }
 }
+
--- Universal x86 Tuning Utility Handheld/Scripts/SMU Backend/RyzenSmu.cs
+++ Universal x86 Tuning Utility Handheld/Scripts/SMU Backend/RyzenSmu.cs
@@ -9,38 +9,350 @@ using System.Threading;
 using System.Threading.Tasks;
 using OpenLibSys;
 using System.Reflection;
+using Universal_x86_Tuning_Utility.Scripts;
 
 [assembly: CLSCompliant(false)]
 
 
 namespace RyzenSmu
 {
-    class Smu
+    class Addresses
     {
-        [DllImport("inpoutx64.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool GetPhysLong(UIntPtr memAddress, out uint Data);
-        public static float ReadFloat(uint Address, uint Offset)
+        public static void setAddresses()
         {
-            uint Data = 0;
-            try
-            {
-                GetPhysLong((UIntPtr)(Address + Offset * 4), out Data);
-            }
-            catch (Exception e)
-            {
-                String ExeptionMSG = $"Error Reading Address 0x{Address:X8} + 0x{Offset:X4}";
-                //MessageBox.Show(ExeptionMSG);
-            }
+            RyzenSmu.Smu.SMU_PCI_ADDR = 0x00000000;
+            RyzenSmu.Smu.SMU_OFFSET_ADDR = 0xB8;
+            RyzenSmu.Smu.SMU_OFFSET_DATA = 0xBC;
 
-            byte[] bytes = new byte[4];
-            bytes = BitConverter.GetBytes(Data);
+            if (Family.FAM == Family.RyzenFamily.SummitRidge || Family.FAM == Family.RyzenFamily.PinnacleRidge) Socket_AM4_V1();
+            if (Family.FAM == Family.RyzenFamily.RavenRidge || Family.FAM == Family.RyzenFamily.Picasso || Family.FAM == Family.RyzenFamily.Dali || Family.FAM == Family.RyzenFamily.Pollock || Family.FAM == Family.RyzenFamily.FireFlight) Socket_FT5_FP5_AM4();
+            if (Family.FAM == Family.RyzenFamily.Matisse || Family.FAM == Family.RyzenFamily.Vermeer) Socket_AM4_V2();
+            if (Family.FAM == Family.RyzenFamily.Renoir || Family.FAM == Family.RyzenFamily.Lucienne || Family.FAM == Family.RyzenFamily.Cezanne_Barcelo) Socket_FP6_AM4();
+            if (Family.FAM == Family.RyzenFamily.VanGogh) Socket_FF3();
+            if (Family.FAM == Family.RyzenFamily.Mendocino || Family.FAM == Family.RyzenFamily.Rembrandt || Family.FAM == Family.RyzenFamily.PhoenixPoint || Family.FAM == Family.RyzenFamily.StrixPoint) Socket_FT6_FP7_FP8();
+            if (Family.FAM == Family.RyzenFamily.Raphael || Family.FAM == Family.RyzenFamily.DragonRange || Family.FAM == Family.RyzenFamily.GraniteRidge || Family.FAM == Family.RyzenFamily.FireRange) Socket_AM5_V1();
 
-            float PmData = BitConverter.ToSingle(bytes, 0);
-            //Console.WriteLine($"0x{Address + Offset * 4,8:X8} | {PmData:F}");
-            return PmData;
         }
 
+        private static void Socket_FT5_FP5_AM4()
+        {
+            RyzenSmu.Smu.MP1_ADDR_MSG = 0x3B10528;
+            RyzenSmu.Smu.MP1_ADDR_RSP = 0x3B10564;
+            RyzenSmu.Smu.MP1_ADDR_ARG = 0x3B10998;
+
+            RyzenSmu.Smu.PSMU_ADDR_MSG = 0x3B10A20;
+            RyzenSmu.Smu.PSMU_ADDR_RSP = 0x3B10A80;
+            RyzenSmu.Smu.PSMU_ADDR_ARG = 0x3B10A88;
+
+            SMUCommands.commands = new List<(string, bool, uint)>
+            {
+                // Store the commands
+                ("stapm-limit",true, 0x1a), // Use MP1 address
+                ("stapm-time",true , 0x1e),
+                ("fast-limit",true , 0x1b),
+                ("slow-limit",true , 0x1c),
+                ("slow-time",true , 0x1d),
+                ("tctl-temp",true , 0x1f),
+                ("cHTC-temp",false , 0x56), // Use RSMU address
+                ("vrm-current",true , 0x20),
+                ("vrmmax-current",true , 0x22),
+                ("vrmsoc-current",true , 0x21),
+                ("vrmsocmax-current",true , 0x23),
+                ("prochot-deassertion-ramp",true , 0x25),
+                ("pbo-scalar",false , 0x68),
+                ("power-saving",true , 0x19),
+                ("max-performance",true , 0x18),
+                ("oc-clk",false , 0x7d),
+                ("per-core-oc-clk",false , 0x7e),
+                ("oc-volt",false , 0x7f),
+                ("enable-oc",false , 0x69),
+                ("disable-oc",false , 0x6a),
+                ("max-cpuclk",true, 0x44),
+                ("min-cpuclk",true, 0x45),
+                ("max-gfxclk",true, 0x46),
+                ("min-gfxclk",true, 0x47),
+                ("max-socclk-frequency",true, 0x48),
+                ("min-socclk-frequency",true, 0x49),
+                ("max-fclk-frequency",true, 0x4a),
+                ("min-fclk-frequency",true, 0x4b),
+                ("max-vcn",true, 0x4c),
+                ("min-vcn",true, 0x4d),
+                ("max-lclk",true, 0x4e),
+                ("min-lclk",true, 0x4f),
+            };
+        }
+
+        private static void Socket_FP6_AM4()
+        {
+            RyzenSmu.Smu.MP1_ADDR_MSG = 0x3B10528;
+            RyzenSmu.Smu.MP1_ADDR_RSP = 0x3B10564;
+            RyzenSmu.Smu.MP1_ADDR_ARG = 0x3B10998;
+
+            RyzenSmu.Smu.PSMU_ADDR_MSG = 0x3B10A20;
+            RyzenSmu.Smu.PSMU_ADDR_RSP = 0x3B10A80;
+            RyzenSmu.Smu.PSMU_ADDR_ARG = 0x3B10A88;
+
+            SMUCommands.commands = new List<(string, bool, uint)>
+            {
+                // Store the commands
+                ("stapm-limit",true , 0x14), // Use MP1 address
+                ("stapm-limit",false , 0x31), // Use RSMU address
+                ("ppt-limit",false , 0x33),
+                ("stapm-time",true , 0x18),
+                ("fast-limit",true , 0x15),
+                ("slow-limit",true , 0x16),
+                ("slow-time",true , 0x17),
+                ("tctl-temp",true , 0x19),
+                ("cHTC-temp",false , 0x37),
+                ("apu-skin-temp",true , 0x38),
+                ("vrm-current",true , 0x1a),
+                ("vrmmax-current",true , 0x1c),
+                ("vrmsoc-current",true , 0x1b),
+                ("vrmsocmax-current",true , 0x1d),
+                ("prochot-deassertion-ramp",true , 0x20),
+                ("gfx-clk",false , 0x89),
+                ("dgpu-skin-temp",true , 0x37),
+                ("power-saving",true , 0x12),
+                ("max-performance",true , 0x11),
+                ("pbo-scalar",false , 0x3F),
+                ("oc-clk",false , 0x19),
+                ("oc-clk",true , 0x31),
+                ("per-core-oc-clk",false , 0x1a),
+                ("per-core-oc-clk",true , 0x32),
+                ("oc-volt",false , 0x1b),
+                ("oc-volt",true , 0x33),
+                ("set-coall",true , 0x55),
+                ("set-coall",false , 0xB1),
+                ("set-coper",true , 0x54),
+                ("set-cogfx",true , 0x64),
+                ("set-cogfx",false , 0x57),
+                ("enable-oc",false , 0x17),
+                ("enable-oc",true , 0x2f),
+                ("disable-oc",false , 0x18),
+                ("disable-oc",true , 0x30)
+            };
+        }
+
+        private static void Socket_FT6_FP7_FP8()
+        {
+            RyzenSmu.Smu.MP1_ADDR_MSG = 0x3B10528;
+            RyzenSmu.Smu.MP1_ADDR_RSP = 0x3B10578;
+            RyzenSmu.Smu.MP1_ADDR_ARG = 0x3B10998;
+
+            RyzenSmu.Smu.PSMU_ADDR_MSG = 0x3B10a20;
+            RyzenSmu.Smu.PSMU_ADDR_RSP = 0x3B10a80;
+            RyzenSmu.Smu.PSMU_ADDR_ARG = 0x3B10a88;
+
+            SMUCommands.commands = new List<(string, bool, uint)>
+            {
+                // Store the commands
+                ("stapm-limit", true, 0x14), // Use MP1 address
+                ("stapm-limit", false, 0x31), // Use RSMU address
+                ("stapm-time", true, 0x18),
+                ("fast-limit", true, 0x15),
+                ("fast-limit", false, 0x32),
+                ("slow-limit", true, 0x16),
+                ("slow-limit", false, 0x33),
+                ("slow-limit", false, 0x34),
+                ("slow-time", true, 0x17),
+                ("tctl-temp", true, 0x19),
+                ("cHTC-temp", false, 0x37),
+                ("apu-skin-temp", true, 0x33),
+                ("vrm-current", true, 0x1a),
+                ("vrmmax-current", true, 0x1c),
+                ("vrmsoc-current", true, 0x1b),
+                ("vrmsocmax-current", true ,0x1d),
+                ("prochot-deassertion-ramp", true, 0x1f),
+                ("gfx-clk", false, 0x89),
+                ("dgpu-skin-temp", true, 0x32),
+                ("power-saving", true, 0x12),
+                ("max-performance", true, 0x11),
+                ("pbo-scalar", false, 0x3E),
+                ("oc-clk",  false, 0x19),
+                ("per-core-oc-clk", false, 0x1a),
+                ("set-coall",   true, 0x4c),
+                ("set-coall",   false, 0x5d),
+                ("set-coper",   true, 0x4b),
+                ("set-cogfx",   false, 0xb7),
+                ("enable-oc",   false, 0x17),
+                ("disable-oc",  false, 0x18)
+            };
+        }
+
+        private static void Socket_FF3()
+        {
+            RyzenSmu.Smu.MP1_ADDR_MSG = 0x3B10528;
+            RyzenSmu.Smu.MP1_ADDR_RSP = 0x3B10578;
+            RyzenSmu.Smu.MP1_ADDR_ARG = 0x3B10998;
+
+            RyzenSmu.Smu.PSMU_ADDR_MSG = 0x3B10a20;
+            RyzenSmu.Smu.PSMU_ADDR_RSP = 0x3B10a80;
+            RyzenSmu.Smu.PSMU_ADDR_ARG = 0x3B10a88;
+
+            SMUCommands.commands = new List<(string, bool, uint)>
+            {
+                // Store the commands
+                ("stapm-limit",true, 0x14), // Use MP1 address
+                ("stapm-limit",false , 0x31), // Use RSMU address
+                ("stapm-time",true , 0x18),
+                ("fast-limit",true , 0x15),
+                ("slow-limit",true , 0x16),
+                ("slow-time",true , 0x17),
+                ("tctl-temp",true , 0x19),
+                ("cHTC-temp",false , 0x37),
+                ("apu-skin-temp",true , 0x33),
+                ("vrm-current",true , 0x1a),
+                ("vrmmax-current",true , 0x1e),
+                ("vrmsoc-current",true , 0x1b),
+                ("vrmsocmax-current",true , 0x1d),
+                ("vrmgfx-current",true , 0x1c),
+                ("vrmgfxmax-current",true , 0x1f),
+                ("prochot-deassertion-ramp",true , 0x22),
+                ("gfx-clk",false , 0x89),
+                ("power-saving",true , 0x12),
+                ("max-performance",true , 0x11),
+                ("set-coall",true , 0x4c),
+                ("set-coall",false , 0x5d),
+                ("set-coper",true , 0x4b),
+                ("set-cogfx",false , 0xb7)
+            };
+        }
+
+        private static void Socket_AM4_V1()
+        {
+            RyzenSmu.Smu.MP1_ADDR_MSG = 0X3B10528;
+            RyzenSmu.Smu.MP1_ADDR_RSP = 0X3B10564;
+            RyzenSmu.Smu.MP1_ADDR_ARG = 0X3B10598;
+
+            RyzenSmu.Smu.PSMU_ADDR_MSG = 0x3B1051C;
+            RyzenSmu.Smu.PSMU_ADDR_RSP = 0X3B10568;
+            RyzenSmu.Smu.PSMU_ADDR_ARG = 0X3B10590;
+
+            SMUCommands.commands = new List<(string, bool, uint)>
+            {
+                // Store the commands
+                ("ppt-limit",false, 0x64), // Use RSMU address
+                ("tdc-limit",false , 0x65),
+                ("edc-limit",false , 0x66),
+                ("tctl-temp",false , 0x68),
+                ("pbo-scalar",false , 0x6a),
+                ("oc-clk", false, 0x6c),
+                ("per-core-oc-clk",false , 0x6d),
+                ("oc-volt", false, 0x6e),
+                ("enable-oc",true , 0x23),
+                ("enable-oc",false , 0x6b),
+                ("disable-oc",true , 0x24),
+            };
+        }
+
+        private static void Socket_AM4_V2()
+        {
+            RyzenSmu.Smu.MP1_ADDR_MSG = 0x3B10530;
+            RyzenSmu.Smu.MP1_ADDR_RSP = 0x3B1057C;
+            RyzenSmu.Smu.MP1_ADDR_ARG = 0x3B109C4;
+
+            RyzenSmu.Smu.PSMU_ADDR_MSG = 0x3B10524;
+            RyzenSmu.Smu.PSMU_ADDR_RSP = 0x3B10570;
+            RyzenSmu.Smu.PSMU_ADDR_ARG = 0x3B10A40;
+
+            SMUCommands.commands = new List<(string, bool, uint)>
+            {
+                // Store the commands
+                ("ppt-limit",true, 0x3D), // Use MP1 address
+                ("ppt-limit",false, 0x53), // Use RSMU address
+                ("tdc-limit",true , 0x3B),
+                ("tdc-limit",false , 0x54),
+                ("edc-limit",true , 0x3c),
+                ("edc-limit",false , 0x55),
+                ("tctl-temp",true , 0x23),
+                ("tctl-temp",false , 0x56),
+                ("pbo-scalar",false , 0x58),
+                ("oc-clk", true, 0x26),
+                ("oc-clk", false, 0x5c),
+                ("per-core-oc-clk",true , 0x27),
+                ("per-core-oc-clk",false , 0x5d),
+                ("oc-volt", true, 0x28),
+                ("oc-volt", false, 0x61),
+                ("set-coall", true, 0x36),
+                ("set-coall", false, 0xb),
+                ("set-coper", true, 0x35),
+                ("enable-oc",true , 0x24),
+                ("enable-oc",false , 0x5a),
+                ("disable-oc",true , 0x25),
+                ("disable-oc",false , 0x5b),
+            };
+        }
+
+        private static void Socket_AM5_V1()
+        {
+            RyzenSmu.Smu.MP1_ADDR_MSG = 0x3010508;
+            RyzenSmu.Smu.MP1_ADDR_RSP = 0x3010988;
+            RyzenSmu.Smu.MP1_ADDR_ARG = 0x3010984;
+
+            RyzenSmu.Smu.PSMU_ADDR_MSG = 0x3B10524;
+            RyzenSmu.Smu.PSMU_ADDR_RSP = 0x3B10570;
+            RyzenSmu.Smu.PSMU_ADDR_ARG = 0x3B10A40;
+
+            SMUCommands.commands = new List<(string, bool, uint)>
+            {
+                // Store the commands
+                ("ppt-limit",true, 0x3e), // Use MP1 address
+                ("ppt-limit",false, 0x56), // Use RSMU address
+                ("tdc-limit",true , 0x3c),
+                ("tdc-limit",false , 0x57),
+                ("edc-limit",true , 0x3d),
+                ("edc-limit",false , 0x58),
+                ("tctl-temp",true , 0x3f),
+                ("tctl-temp",false , 0x59),
+                ("pbo-scalar",false , 0x5b),
+                ("oc-clk", false, 0x5f),
+                ("per-core-oc-clk",false , 0x60),
+                ("oc-volt", false, 0x61),
+                ("set-coall", false, 0x7),
+                ("set-coper", false, 0x6),
+                ("enable-oc",false , 0x5d),
+                ("disable-oc",false , 0x5e),
+            };
+        }
+    }
+
+    class SMUCommands
+    {
+        public static List<(string, bool, uint)> commands;
+
+        public static Smu RyzenAccess = new Smu(false);
+
+        public static void applySettings(string commandName, uint value)
+        {
+            RyzenAccess.Initialize();
+            uint[] Args = new uint[6];
+            Args[0] = value;
+
+            // Find the command by name
+            var matchingCommands = commands.Where(c => c.Item1 == commandName);
+            if (matchingCommands.Count() > 0)
+            {
+                List<Task> tasks = new List<Task>();
+                foreach (var command in matchingCommands)
+                {
+                    tasks.Add(Task.Run(async () =>
+                    {
+                        // Apply the command based on its address
+                        if (command.Item2 == true) RyzenAccess.SendMp1(command.Item3, ref Args);
+                        else RyzenAccess.SendRsmu(command.Item3, ref Args);
+                    }));
+                }
+
+                Task.WaitAll(tasks.ToArray());
+            }
+            else throw new ArgumentException($"Command '{commandName}' not found");
+
+            RyzenAccess.Deinitialize();
+        }
+    }
+
+    class Smu
+    {
         public enum Status : int
         {
             BAD = 0x0,
@@ -61,10 +373,7 @@ namespace RyzenSmu
             { Smu.Status.CMD_REJECTED_BUSY, "CMD Rejected Busy" }
         };
 
-
-
         Ols RyzenNbAccesss;
-
 
         public Smu(bool EnableDebug)
         {
@@ -137,15 +446,6 @@ namespace RyzenSmu
                 default:
                     break;
             }
-        }
-        public uint GetCpuId()
-        {
-            uint eax = 0, ebx = 0, ecx = 0, edx = 0;
-            if (RyzenNbAccesss.Cpuid(0x00000001, ref eax, ref ebx, ref ecx, ref edx) == 1)
-            {
-                return eax;
-            }
-            return 0;
         }
 
         public void Deinitialize()
@@ -273,68 +573,6 @@ namespace RyzenSmu
                 return RyzenNbAccesss.ReadPciConfigDwordEx(SMU_PCI_ADDR, SMU_OFFSET_DATA, ref data) == 1;
             }
             return false;
-        }
-
-        private uint ReadDword(uint value)
-        {
-            RyzenNbAccesss.WritePciConfigDword(SMU_PCI_ADDR, (byte)SMU_OFFSET_ADDR, value);
-            return RyzenNbAccesss.ReadPciConfigDword(SMU_PCI_ADDR, (byte)SMU_OFFSET_DATA);
-        }
-
-
-        private bool Wait4Rsp(uint SMU_ADDR_RSP)
-        {
-            bool res = false;
-            ushort timeout = 1000;
-            uint data = 0;
-            while ((!res || data == 0) && --timeout > 0)
-            {
-                res = SmuReadReg(SMU_ADDR_RSP, ref data);
-                Thread.Sleep(1);
-            }
-
-            //Console.WriteLine($"Time{(timeout):D}: 0x{data,8:X8}");
-
-
-            if (timeout == 0 || data != 1) res = false;
-            //Console.WriteLine($"Res{(res):D}");
-
-
-            return res;
-        }
-
-
-        //Code by I.nfraR.ed to get the CpuID
-        private string GetStringPart(uint val)
-        {
-            return val != 0 ? Convert.ToChar(val).ToString() : "";
-        }
-
-        private string IntToStr(uint val)
-        {
-            uint part1 = val & 0xff;
-            uint part2 = val >> 8 & 0xff;
-            uint part3 = val >> 16 & 0xff;
-            uint part4 = val >> 24 & 0xff;
-
-            return string.Format("{0}{1}{2}{3}", GetStringPart(part1), GetStringPart(part2), GetStringPart(part3), GetStringPart(part4));
-        }
-
-        public string GetCpuName()
-        {
-            string model = "";
-            uint eax = 0, ebx = 0, ecx = 0, edx = 0;
-
-            if (RyzenNbAccesss.Cpuid(0x80000002, ref eax, ref ebx, ref ecx, ref edx) == 1)
-                model = model + IntToStr(eax) + IntToStr(ebx) + IntToStr(ecx) + IntToStr(edx);
-
-            if (RyzenNbAccesss.Cpuid(0x80000003, ref eax, ref ebx, ref ecx, ref edx) == 1)
-                model = model + IntToStr(eax) + IntToStr(ebx) + IntToStr(ecx) + IntToStr(edx);
-
-            if (RyzenNbAccesss.Cpuid(0x80000004, ref eax, ref ebx, ref ecx, ref edx) == 1)
-                model = model + IntToStr(eax) + IntToStr(ebx) + IntToStr(ecx) + IntToStr(edx);
-
-            return model.Trim();
         }
     }
 }
